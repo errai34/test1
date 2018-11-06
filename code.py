3519b4844c0e1a2a79f5e71b1640be6ae82bc292
errai34 <ioana.ciuca034@gmail.com>
2018-11-06 15:31:27 -0600

    added that Ioana was there!

--- code.py
+++ code.py
@@ -3,3 +3,6 @@ def do_something(x):
     sq_x=x**2
     cub_x=x**3
     print(sq_x, cub_x)# this will make it much easier in future problems to see that something is actually happening
+    print("Ioana was here!")
+
+
